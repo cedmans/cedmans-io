27d394274cfbaa56971ab7727bb787bc8f9deaf7
Cedric Wienold <cwienold@gmail.com>
2016-10-18 16:33:43 -0700

    Set up cache busting in gulpfile/elixir

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ require('laravel-elixir-vue-2');
 elixir(mix => {
     mix.sass('app.scss')
        .webpack('app.js')
+       .version(['css/app.css', 'js/app.js'])
        .browserSync({
            proxy: 'localhost:8000'
        })
