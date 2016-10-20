e548e01ee54e527f561d609330d9becc6572fd91
Cedric Wienold <cwienold@gmail.com>
2016-10-19 17:30:03 -0700

    Redo gradient location to allow content afterwards

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -1,6 +1,13 @@
 <template>
     <section class="hero">
-        <div class="hero-image"></div>
+        <div class="hero-image">
+        </div>
+        <div class="row intro">
+            <div class="small-centered medium-uncentered medium-6 large-7 columns">
+                <h1>Yo Dawg</h1>
+                <p>Here I am!</p>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -16,6 +23,7 @@
         .hero-image {
             background-image: url('/images/desk-hero-small.jpg');
             height: 100vh;
+            width: 100%;
             -webkit-filter: blur(5px);
             -moz-filter: blur(5px);
             -o-filter: blur(5px);
@@ -27,17 +35,20 @@
             -ms-transform: scale(1.1);
             -o-transform: scale(1.1);
             transform: scale(1.1);
-        }
 
-        &:after {
-            content: '';
             position: absolute;
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-            background-image: linear-gradient(to bottom right, #004250, #04004d);
-            opacity: .8;
+            z-index: -1;
+
+            &:after {
+                content: '';
+                position: absolute;
+                top: 0;
+                right: 0;
+                bottom: 0;
+                left: 0;
+                background-image: linear-gradient(to bottom right, #004250, #04004d);
+                opacity: .8;
+            }
         }
     }
 </style>
