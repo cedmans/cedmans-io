eeb781ab92d7fd6bfe4c415221751bc21153304a
Cedric Wienold <cwienold@gmail.com>
2016-10-20 13:03:00 -0700

    Extract hero section into its own component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,9 +13,7 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
 Vue.component('app', require('./components/App.vue'));
-
 const app = new Vue({
     el: '#app'
 });
---
+++ resources/assets/js/components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="hero">
+        <div class="hero-image"></div>
+        <div class="row intro">
+            <div class="columns hide-for-medium">
+                <div class="face-image"></div>
+            </div>
+            <div class="medium-7 large-5 large-offset-1 columns">
+                <h1>Cedric Wienold</h1>
+                <p>Engineering, Architecture, and Artistry in Software</p>
+            </div>
+            <div class="show-for-medium medium-5 large-2 columns end">
+                <div class="face-image"></div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {}
+</script>
