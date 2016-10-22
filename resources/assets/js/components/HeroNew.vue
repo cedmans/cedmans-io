6ab3674f05f2a6c26dc193a15a24a8c214fffb85
Cedric Wienold <cwienold@gmail.com>
2016-10-21 17:49:42 -0700

    Style buttons in new hero

--- resources/assets/js/components/HeroNew.vue
+++ resources/assets/js/components/HeroNew.vue
@@ -8,6 +8,16 @@
             <h1>{{ user.name }}</h1>
             <p>Engineering, Architecture, and Artistry in Software</p>
         </div>
+        <div class="row hero-buttons">
+            <div class="columns">
+                <div class="button-group large stacked">
+                    <a href="#" class="button hollow white round">My Mission</a>
+                    <a href="#" class="button hollow white round">Musings</a>
+                    <a href="#" class="button hollow white round">LinkedIn</a>
+                    <a href="#" class="button hollow white round">Github</a>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
