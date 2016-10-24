6a636535d56c7ff850cc4e7eefe2de4d969e9c58
Cedric Wienold <cwienold@gmail.com>
2016-10-24 00:43:59 -0700

    Refactor welcome view into layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}" />
+
+        <title>@yield('title', 'Cedmans.IO')</title>
+    </head>
+    <body>
+        <div id="app" v-cloak>
+            @yield('content')
+        </div>
+
+        <script>
+            window.Laravel = { csrfToken: '{{ csrf_token() }}'};
+        </script>
+        <script type="text/javascript" src="{{ elixir('js/app.js') }}"></script>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts/master')
 
-        <link rel="stylesheet" href="{{ elixir('css/app.css') }}" />
-
-        <title>Cedmans.IO</title>
-    </head>
-    <body>
-        <div id="app" v-cloak>
-            <app></app>
-        </div>
-
-        <script>
-            window.Laravel = { csrfToken: '{{ csrf_token() }}'};
-        </script>
-        <script type="text/javascript" src="{{ elixir('js/app.js') }}"></script>
-    </body>
-</html>
+@section('content')
+    <app></app>
+@endsection
